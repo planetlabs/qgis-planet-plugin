aa257a683773141a900af738ef351f1f8a12595c
volaya <volayaf@gmail.com>
2021-07-22 12:17:47 +0200

    changed message of mac 3.20 warning

--- planet_explorer/pe_plugin.py
+++ planet_explorer/pe_plugin.py
@@ -552,11 +552,14 @@ class PlanetExplorer(object):
 
     def login(self):
         if Qgis.QGIS_VERSION_INT >= 32000 and platform.system() == "Darwin":
-            text = ("WARNING: Your configuration may encounter serious issues with the Planet QGIS Plugin."
-                    " We are actively troubleshooting the issue with the QGIS team, you can track Issue 44182"
-                    " <a href='https://github.com/qgis/QGIS/issues/44182'>here</a>. "
-                    " In the meantime, we recommend that you use a QGIS version between 3.10 and 3.20,"
-                    " such as the 3.16 long term stable release.")
+            text = ("WARNING: Your configuration may encounter serious issues with the Planet QGIS Plugin "
+                    "using QGIS V3.20. We are actively troubleshooting the issue with the QGIS team, you "
+                    "can track <a href='https://github.com/qgis/QGIS/issues/44182'>Issue 44182 here</a>. "
+                    "In the meantime, we recommend that you use a QGIS version between 3.10 and 3.20, "
+                    "such as the 3.16 long term stable release. For further information including instructions "
+                    "on how to downgrade QGIS, please refer to our "
+                    "<a href='https://support.planet.com/hc/en-us/articles/4404372169233'>support page here</a>."
+                    )
             QMessageBox.warning(self.iface.mainWindow(), "Planet Explorer", text)
         show_explorer()
 
