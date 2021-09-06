c565d5be1128a26eb3b42e24623140db3a72530a
volaya <volayaf@gmail.com>
2021-09-06 11:13:17 +0200

    added dummy iface for testing

--- planet_explorer/tests/test_plugin.py
+++ planet_explorer/tests/test_plugin.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from qgis import utils
-from qgis.testing import start_app, unittest
-
-# from planet_explorer.gui.pe_explorer_dockwidget import show_explorer
-from planet_explorer.tests import testinterface
-
-start_app()
-
-utils.iface = testinterface.iface
-
+from qgis.testing import unittest
 
 class TestPlugin(unittest.TestCase):
     def test_import_planet(self):
---
+++ planet_explorer/tests/utils.py
@@ -0,0 +1,13 @@
+from qgis.testing import start_app
+from qgis import utils
+
+class QgisInterfaceDummy(object):
+    def __getattr__(self, name):
+        # return an function that accepts any arguments and does nothing
+        def dummy(*args, **kwargs):
+            return None
+        return dummy
+
+if utils.iface is None:
+	utils.iface = QgisInterfaceDummy()
+	start_app()
