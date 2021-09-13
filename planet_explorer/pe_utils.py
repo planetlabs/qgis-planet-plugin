18d3ec6053d566c12e699cf101501055c5f72f13
volaya <volayaf@gmail.com>
2021-09-13 07:53:04 +0200

    fixed issue with default value of downloads folder

--- planet_explorer/pe_utils.py
+++ planet_explorer/pe_utils.py
@@ -418,7 +418,8 @@ def resource_file(f):
 
 def orders_download_folder():
     download_folder = QSettings().value(
-        f"{SETTINGS_NAMESPACE}/{ORDERS_DOWNLOAD_FOLDER_SETTING}"
+        f"{SETTINGS_NAMESPACE}/{ORDERS_DOWNLOAD_FOLDER_SETTING}",
+        ""
     )
     if not os.path.exists(download_folder):
         try:
