351756f37e1c140fdeb55a8d8c3c5f301cb483b9
volaya <volayaf@gmail.com>
2021-09-15 10:16:50 +0200

    more tests

--- planet_explorer/tests/test_plugin.py
+++ planet_explorer/tests/test_plugin.py
@@ -2,12 +2,11 @@
 
 
 from qgis.testing import unittest, start_app
-from planet_explorer.gui.pe_explorer_dockwidget import (
-    PlanetExplorerDockWidget
-)
+from planet_explorer.gui.pe_explorer_dockwidget import PlanetExplorerDockWidget
 from planet_explorer.pe_utils import iface
-from planet_explorer.tests.utils import patch_iface, get_testing_credentials
+from planet_explorer.tests.utils import patch_iface, get_testing_credentials, test_aoi
 from planet_explorer.planet_api import PlanetClient
+
 start_app()
 
 
@@ -24,6 +23,7 @@ class TestPlugin(unittest.TestCase):
     def test_import_planet(self):
         try:
             import planet  # noqa: F401
+
             assert True
         except ImportError:
             assert False
@@ -58,3 +58,28 @@ class TestPlugin(unittest.TestCase):
         PlanetClient.getInstance().log_in(user, password)
         current = self.dockwidget.stckdWidgetViews.currentIndex()
         assert current == 1
+
+    def test_can_search_daily_images(self):
+        user, password = get_testing_credentials()
+        PlanetClient.getInstance().log_in(user, password)
+        current = self.dockwidget.stckdWidgetViews.currentIndex()
+        assert current == 1
+        self.dockwidget.daily_images_widget._main_filters.leAOI.setText(test_aoi())
+        self.dockwidget.daily_images_widget.perform_search()
+        assert (
+            self.dockwidget.daily_images_widget.searchResultsWidget.tree.topLevelItemCount()
+            == 80
+        )
+
+    def test_basemaps_are_shown(self):
+        user, password = get_testing_credentials()
+        PlanetClient.getInstance().log_in(user, password)
+        current = self.dockwidget.stckdWidgetViews.currentIndex()
+        assert current == 1
+        self.dockwidget.show_mosaics_panel()
+        current = self.dockwidget.tabWidgetResourceType.currentIndex()
+        assert current == 1
+        assert self.dockwidget.basemaps_widget.mosaicsList.count() == 0
+        assert self.dockwidget.basemaps_widget.comboSeriesName.count() > 1
+        self.dockwidget.basemaps_widget.comboSeriesName.setCurrentIndex(1)
+        assert self.dockwidget.basemaps_widget.mosaicsList.count() > 0
--- planet_explorer/tests/utils.py
+++ planet_explorer/tests/utils.py
@@ -7,5 +7,13 @@ def patch_iface():
     pe_utils.iface.messageTimeout.return_value = 5
 
 
+def test_aoi():
+    return (
+        '{"coordinates":[[[-0.334369,40.151264],[-0.276291,40.151264],'
+        "[-0.276291,40.172081],[-0.334369,40.172081],[-0.334369,40.151264]]]"
+        ',"type":"Polygon"}'
+    )
+
+
 def get_testing_credentials():
     return os.environ["PLANET_USER"], os.environ["PLANET_PASSWORD"]
