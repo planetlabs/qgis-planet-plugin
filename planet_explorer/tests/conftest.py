347c728601b626a334a77f2e05f52af5ca486730
John Dupuy <john.dupuy@planet.com>
2022-02-25 10:52:06 -0800

    Convert unit tests to use pytest-qgis and pytest-qt

---
+++ planet_explorer/tests/conftest.py
@@ -0,0 +1,88 @@
+import pytest
+
+from planet_explorer import classFactory
+from qgis.PyQt import QtCore
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_addoption(parser) -> None:
+    """Add some custom ini values"""
+    parser.addini(
+        "qgis_window_height",
+        "Set the window height for QGIS",
+        type="string",
+        default="800",
+    )
+    parser.addini(
+        "qgis_window_width",
+        "Set the window width for QGIS",
+        type="string",
+        default="800",
+    )
+
+
+@pytest.fixture(scope="session")
+def qgis_debug_enabled(request, pytestconfig):
+    gui_enabled = request.config._plugin_settings.gui_enabled
+    yield gui_enabled
+
+
+@pytest.fixture
+def pe_qgis_iface(qgis_iface):
+    """
+    Patch the pytest-qgis's qgis_iface to include some specific methods
+    for the Planet Explorer plugin.
+    """
+
+    def dummy_method(*args, **kwargs):
+        pass
+
+    class DummyClass:
+        pass
+
+    for method in [
+        "addPluginToWebMenu",
+        "messageTimeout",
+        "removePluginWebMenu",
+        "removeDockWidget",
+    ]:
+        setattr(qgis_iface, method, dummy_method)
+
+    qgis_iface.layerTreeView = DummyClass
+    qgis_iface.layerTreeView.layerTreeModel = DummyClass
+    qgis_iface.layerTreeView.layerTreeModel.refreshLayerLegend = dummy_method
+    qgis_iface.layerTreeView.currentNode = DummyClass
+    qgis_iface.layerTreeView.currentNode.setExpanded = dummy_method
+    qgis_iface.messageTimeout.return_value = 5
+    yield qgis_iface
+
+
+@pytest.fixture
+def plugin(
+    pytestconfig, pe_qgis_iface, qgis_parent, qgis_new_project, qgis_debug_enabled
+):
+    """
+    Initialize and return the plugin object.
+
+    Resize the parent window according to config.
+    """
+    qgis_parent.resize(
+        QtCore.QSize(
+            int(pytestconfig.getini("qgis_window_width")),
+            int(pytestconfig.getini("qgis_window_height")),
+        )
+    )
+
+    plugin = classFactory(pe_qgis_iface)
+    plugin.initGui()
+    yield plugin
+    plugin.unload()
+
+
+@pytest.fixture
+def sample_aoi():
+    yield (
+        '{"coordinates":['
+        "[[-0.334369,40.151264],[-0.276291,40.151264],[-0.276291,40.172081],"
+        '[-0.334369,40.172081],[-0.334369,40.151264]]],"type":"Polygon"}'
+    )
