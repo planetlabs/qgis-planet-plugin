77977638c07f41279b876291dd8391cd64b24b50
John Dupuy <john.dupuy@planet.com>
2022-03-01 15:35:36 -0800

    Add a small install script for testing plugin install

---
+++ planet_explorer/tests/install_plugin.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+"""
+Install a plugin from a zip file into QGIS. This script is meant to be run inside of QGIS:
+
+.. code-block:: bash
+
+    qgis --noplugins --code install_plugin.py
+
+The package zip file for the plugin is expected to be present in current working directory.
+"""
+import os
+import pathlib
+import traceback
+
+
+class PluginInstallException(Exception):
+    pass
+
+
+try:
+    try:
+        import pyplugin_installer
+    except ImportError:
+        raise PluginInstallException(
+            "Cannot install plugin as 'pyplugin_installer' could not be imported."
+            " Is the script running in the QGIS env?"
+        )
+
+    zip_files = [file for file in pathlib.Path("./").glob("*.zip")]
+
+    if not zip_files:
+        raise PluginInstallException(
+            f"ERROR: No plugin zip file found at {pathlib.Path('./').absolute()}."
+        )
+
+    if len(zip_files) > 1:
+        raise PluginInstallException(
+            f"ERROR: More than one plugin zip file found at {pathlib.Path('./').absolute()}."
+            f" Found {[str(f) for f in zip_files]}."
+        )
+
+    plugin_install_zip = zip_files[0]
+    plugin_installer = pyplugin_installer.instance()
+
+    # Install from the zip file
+    plugin_installer.installFromZipFile(str(plugin_install_zip.absolute()))
+    assert "planet_explorer" in pyplugin_installer.installer_data.plugins.all().keys()
+
+    # Uninstall the plugin
+    plugin_installer.uninstallPlugin("planet_explorer", quiet=True)
+    assert (
+        "planet_explorer" not in pyplugin_installer.installer_data.plugins.all().keys()
+    )
+except Exception:  # noqa
+    # Print the error so we know where it failed,
+    # and exit with a non-zero status code so CI will fail.
+    print("FAIL: Plugin install and uninstalled failed with the following:")
+    print(traceback.format_exc())
+    os._exit(1)
+finally:
+    # The install and uninstall worked! Exit QGIS with a 0 status code.
+    print(
+        f"PASS: Plugin install and uninstall successful for {str(plugin_install_zip)}"
+    )
+    os._exit(0)
