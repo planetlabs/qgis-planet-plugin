4b5db73fb8a4b65e9afb8c760a65cd33f2e1dfdd
John Dupuy <john.dupuy@planet.com>
2022-03-03 13:33:14 -0800

    Add exception handling so we catch python errors

--- planet_explorer/tests/install_plugin.py
+++ planet_explorer/tests/install_plugin.py
@@ -7,6 +7,10 @@ Install a plugin from a zip file into QGIS. This script is meant to be run insid
     qgis --noplugins --code install_plugin.py
 
 The package zip file for the plugin is expected to be present in current working directory.
+
+Verifies:
+    - PLQGIS-TC01
+    - PLQGIS-TC02
 """
 import os
 import pathlib
@@ -17,9 +21,24 @@ class PluginInstallException(Exception):
     pass
 
 
+ERROR_OCCURRED = False
+ERROR_MSG = ""
+
+
+def error_catcher(msg, tag, level):
+    """
+    Catch a python error and raise a PluginInstallException
+    """
+    global ERROR_OCCURRED, ERROR_MSG
+    if tag == "Python error" and level != 0:
+        ERROR_OCCURRED = True
+        ERROR_MSG = msg
+
+
 try:
     try:
         import pyplugin_installer
+        from qgis.core import QgsApplication
     except ImportError:
         raise PluginInstallException(
             "Cannot install plugin as 'pyplugin_installer' could not be imported."
@@ -41,16 +60,30 @@ try:
 
     plugin_install_zip = zip_files[0]
     plugin_installer = pyplugin_installer.instance()
+    # Make sure plugin is not installed
+    assert (
+        "planet_explorer" not in pyplugin_installer.installer_data.plugins.all().keys()
+    ), "Planet plugin is already installed!"
+
+    # Attach the error catcher
+    QgsApplication.messageLog().messageReceived.connect(error_catcher)
 
     # Install from the zip file
     plugin_installer.installFromZipFile(str(plugin_install_zip.absolute()))
-    assert "planet_explorer" in pyplugin_installer.installer_data.plugins.all().keys()
+    assert (
+        "planet_explorer" in pyplugin_installer.installer_data.plugins.all().keys()
+    ), "Planet plugin failed to install!"
+
+    if ERROR_OCCURRED:
+        raise PluginInstallException(
+            f"Python exception hit during plugin install: \n {ERROR_MSG}"
+        )
 
     # Uninstall the plugin
     plugin_installer.uninstallPlugin("planet_explorer", quiet=True)
     assert (
         "planet_explorer" not in pyplugin_installer.installer_data.plugins.all().keys()
-    )
+    ), "Planet plugin failed to uninstall!"
 except Exception:  # noqa
     # Print the error so we know where it failed,
     # and exit with a non-zero status code so CI will fail.
