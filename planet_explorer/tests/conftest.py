9f13f953a3de21edec82aa7be3f80f846a1d31b5
John Dupuy <john.dupuy@planet.com>
2022-03-11 10:48:26 -0800

    Adjust for running tests in docker

--- planet_explorer/tests/conftest.py
+++ planet_explorer/tests/conftest.py
@@ -1,8 +1,19 @@
+import os
 import pytest
 
 from planet_explorer import classFactory
 from unittest.mock import MagicMock
 from qgis.PyQt import QtCore
+from qgis.testing import start_app
+
+
+if os.environ.get("IS_DOCKER_CONTAINER") and os.environ["IS_DOCKER_CONTAINER"].lower()[
+    0
+] in ["t", "y", "1"]:
+    # when running in a docker container, we use the start_app provided by qgis rather
+    # than that of pytest-qgis. pytest-qgis does not cleanup the application properly
+    # and results in a seg-fault
+    start_app()
 
 
 @pytest.hookimpl(tryfirst=True)
