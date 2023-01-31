97209c0b6138583a6a9c3fe6fe103fee27df4089
Samweli <smwltwesa6@gmail.com>
2023-02-01 00:47:17 +0300

    reformatted test_plugin file

--- planet_explorer/tests/test_plugin.py
+++ planet_explorer/tests/test_plugin.py
@@ -6,8 +6,10 @@ import xml.etree.ElementTree as ET
 
 REPO_URL = "https://api.github.com/repos/planetlabs/qgis-planet-plugin/"
 
-CUSTOM_REPOSITORY_URL = 'https://raw.githubusercontent.com/planetlabs/qgis-planet-plugin' \
-                        '/release/docs/repository/plugins.xml'
+CUSTOM_REPOSITORY_URL = (
+    "https://raw.githubusercontent.com/planetlabs/qgis-planet-plugin"
+    "/release/docs/repository/plugins.xml"
+)
 
 
 def test_import_planet():
@@ -42,20 +44,29 @@ def test_plugin_repository_content():
     assert repo_content_resp.status_code == 200
 
     root = ET.fromstring(repo_content_resp.text)
-    plugin = root.find('pyqgis_plugin')
+    plugin = root.find("pyqgis_plugin")
 
     assert plugin is not None
-    assert plugin.find('qgis_minimum_version').text == '3.10'
-    assert plugin.find('icon').text == 'resources/planet-logo-p.png'
+    assert plugin.find("qgis_minimum_version").text == "3.10"
+    assert plugin.find("icon").text == "resources/planet-logo-p.png"
 
-    assert plugin.find('homepage').text == 'https://developers.planet.com/docs/integrations/qgis/'
-    assert plugin.find('repository').text == 'https://github.com/planetlabs/qgis-planet-plugin'
-    assert plugin.find('tracker').text == 'https://github.com/planetlabs/qgis-planet-plugin/issues'
-    assert plugin.find('tags').text == 'landsat, raster, analytics, remote sensing'
+    assert (
+        plugin.find("homepage").text
+        == "https://developers.planet.com/docs/integrations/qgis/"
+    )
+    assert (
+        plugin.find("repository").text
+        == "https://github.com/planetlabs/qgis-planet-plugin"
+    )
+    assert (
+        plugin.find("tracker").text
+        == "https://github.com/planetlabs/qgis-planet-plugin/issues"
+    )
+    assert plugin.find("tags").text == "landsat, raster, analytics, remote sensing"
 
-    assert plugin.find('file_name').text is not None
-    assert plugin.find('version') is not None
-    assert plugin.find('experimental') is not None
-    assert plugin.find('deprecated') is not None
-    assert plugin.find('about') is not None
-    assert plugin.find('description') is not None
+    assert plugin.find("file_name").text is not None
+    assert plugin.find("version") is not None
+    assert plugin.find("experimental") is not None
+    assert plugin.find("deprecated") is not None
+    assert plugin.find("about") is not None
+    assert plugin.find("description") is not None
