7a18fa5fb2af8c7a6d7802c1a07d63f39160095b
Samweli <smwltwesa6@gmail.com>
2023-02-01 00:36:41 +0300

    test for the planet custom plugin repository

--- planet_explorer/tests/test_plugin.py
+++ planet_explorer/tests/test_plugin.py
@@ -2,7 +2,12 @@ import requests
 from urllib.parse import urljoin
 from planet_explorer.tests.utils import get_recent_release_from_changelog
 
+import xml.etree.ElementTree as ET
+
 REPO_URL = "https://api.github.com/repos/planetlabs/qgis-planet-plugin/"
+
+CUSTOM_REPOSITORY_URL = 'https://raw.githubusercontent.com/planetlabs/qgis-planet-plugin' \
+                        '/release/docs/repository/plugins.xml'
 
 
 def test_import_planet():
@@ -29,3 +34,28 @@ def test_changelog_up_to_date(request):
     assert (
         most_recent_release_in_changelog == most_recent_release_on_github
     ), "Release on Github does not match the most recent changelog entry!"
+
+
+def test_plugin_repository_content():
+    repo_content_resp = requests.get(CUSTOM_REPOSITORY_URL)
+
+    assert repo_content_resp.status_code == 200
+
+    root = ET.fromstring(repo_content_resp.text)
+    plugin = root.find('pyqgis_plugin')
+
+    assert plugin is not None
+    assert plugin.find('qgis_minimum_version').text == '3.10'
+    assert plugin.find('icon').text == 'resources/planet-logo-p.png'
+
+    assert plugin.find('homepage').text == 'https://developers.planet.com/docs/integrations/qgis/'
+    assert plugin.find('repository').text == 'https://github.com/planetlabs/qgis-planet-plugin'
+    assert plugin.find('tracker').text == 'https://github.com/planetlabs/qgis-planet-plugin/issues'
+    assert plugin.find('tags').text == 'landsat, raster, analytics, remote sensing'
+
+    assert plugin.find('file_name').text is not None
+    assert plugin.find('version') is not None
+    assert plugin.find('experimental') is not None
+    assert plugin.find('deprecated') is not None
+    assert plugin.find('about') is not None
+    assert plugin.find('description') is not None
